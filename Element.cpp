9ed0845457d456298f01b310d796278e1fc7d69c
nhphan0505 <69851474+nhphan0505@users.noreply.github.com>
2021-02-24 16:59:09 +0700

    Update Element.cpp

--- Element.cpp
+++ Element.cpp
@@ -3,15 +3,15 @@ using namespace std;
 #define ll long long
 
 int main(){
-    ll p,k,du,ans=1,luu[10000];
+    ll p,k,du,ans=1,luu[10000]; // dùng mảng "luu" để lưu các số dư, vì khi số dư lặp lại thì số đó không thể chia hết
     cin>>p>>k;
     du=p%k;
     luu[du]++;
-    while(du!=0){
+    while(1==1){
         du=(du*10+p)%k;
-        if(luu[du]!=0){
-            cout<<"-1";
-            return 0;
+        if(luu[du]!=0){//nếu số dư lặp lại
+            cout<<"-1"; //in ra -1
+            return 0;// dừng chương trình
         } 
         luu[du]++;
         ans++;
