0b15d516ac6db06d227acdf2b8c156a4fdbd937a
nhphan0505 <69851474+nhphan0505@users.noreply.github.com>
2021-02-24 16:53:17 +0700

    Update New-characters.cpp

--- New-characters.cpp
+++ New-characters.cpp
@@ -9,7 +9,7 @@ using namespace std;
 int main() {
     char a;
     cin>>a;
-    a=a+32;
+    a=a+32; //trong bảng mã ascii thì chữ cái thường các chữ cái hoa 32 đơn vị, tham khảo thêm tại https://en.wikipedia.org/wiki/File:ASCII-Table.svg
     cout<<a;
     return 0;
 }
