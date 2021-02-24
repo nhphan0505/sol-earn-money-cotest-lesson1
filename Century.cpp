1a893cc2b20a9a0dfba65a23bbad00aae6066f00
nhphan0505 <69851474+nhphan0505@users.noreply.github.com>
2021-02-24 16:49:47 +0700

    Update Century.cpp

--- Century.cpp
+++ Century.cpp
@@ -9,7 +9,7 @@ using namespace std;
 int main() {
     int a;
     cin>>a;
-    if(a%100==0) cout<<a/100;
-    else cout<<a/100+1;
+    if(a%100==0) cout<<a/100;  //nếu năm đó chia hết cho 100 thì bỏ đi 2 số cuối của năm đó chính là thế kỉ (ví dụ 1900, 2000, 1800)
+    else cout<<a/100+1; //nếu ko thì bỏ đi 2 số cuối rồi công vs 1 của năm đó chính là thế kỉ (ví dụ 1901, 2002, 1803)
     return 0;
 }
