a4c401f56b17916de6c5779e85e28e30b6183c1f
nhphan0505 <69851474+nhphan0505@users.noreply.github.com>
2021-02-24 16:42:21 +0700

    Create New-characters.cpp

---
+++ New-characters.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    char a;
+    cin>>a;
+    a=a+32;
+    cout<<a;
+    return 0;
+}
